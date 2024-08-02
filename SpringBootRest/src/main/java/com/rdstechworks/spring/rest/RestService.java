78430c7d0c7fec9c68391214932e19dd6599c133
rdstechworks <rdstechworks@gmail.com>
2024-08-02 19:51:02 +0530

    Spring boot project for Rest operations

---
+++ SpringBootRest/src/main/java/com/rdstechworks/spring/rest/RestService.java
@@ -0,0 +1,14 @@
+package com.rdstechworks.spring.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestService {
+
+	@GetMapping("/")
+	public String start() {
+		
+		return "Hello!! This is Rest service example";
+	}
+}
